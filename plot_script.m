e86bc55497f390d8b1958186aceb48a9d5a13057
Jayant Jain <jayantjain1992@gmail.com>
2015-03-29 20:42:04 +0530

    Surface and trajectory show up on the same figure now

--- plot_script.m
+++ plot_script.m
@@ -16,15 +16,15 @@ b1_plot = 0.50;
 
 % h(1) = figure;
 figure;
+z = A1_plot*sin(a1_plot*x) + B1_plot*cos(b1_plot*y);	
+ezplot3(x,y,z, [1, 10]);%plots traj 
+
+hold on;
 [x_plot,y_plot] = meshgrid(0:.1:10);
 Z = A1_plot*sin(a1_plot*x_plot) + B1_plot*cos(b1_plot*y_plot);	
 surf(x_plot, y_plot, Z);%plots surface
-
-%hold on;
 % h(2) = figure;
-figure;
-z = A1_plot*sin(a1_plot*x) + B1_plot*cos(b1_plot*y);	
-ezplot3(x,y,z, [1, 10]);%plots traj 
+% figure;
 
 end
 
